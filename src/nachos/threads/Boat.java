f30557b85dbac8ce8e29372624b9ece5f07b44d5
FlameYan <yan-zp@foxmail.com>
2015-04-14 01:23:04 +0800

    haha

--- src/nachos/threads/Boat.java
+++ src/nachos/threads/Boat.java
@@ -18,18 +18,70 @@ public class Boat
 //  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
 //  	begin(3, 3, b);
     }
-
-    public static void begin( int adults, int children, BoatGrader b )
+    
+    //////Global variables
+    static int BoatLocation; // 0 for Oahu, 1 for Molokai
+    static int Weight; // 50kg per child, 100kg per adult  on boat
+    static int Total; // number of all people
+    static int OnMolokai; // number of people get in Molokai
+    static int ChOahu; // number of children on Oahu
+    static Lock BoatLock = new Lock();
+    static Condition WaitOahu = new Condition(BoatLock);
+    static Condition WaitMolokai = new Condition(BoatLock);
+    static Condition WaitFull = new Condition(BoatLock);
+    //////Global variables
+    
+    public static void begin( int adults, int children, BoatGrader b ) 
     {
 	// Store the externally generated autograder in a class
 	// variable to be accessible by children.
 	bg = b;
 
 	// Instantiate global variables here
+	BoatLocation = 0;
+	Weight = 0;
+	Total = adults + children;
+	OnMolokai = 0;
+	ChOahu = children;
+	
+	
 	
 	// Create threads here. See section 3.4 of the Nachos for Java
+	
+	Runnable cr = new Runnable() {
+	    public void run() {
+	    		ChildItinerary(0);
+            }
+        };
+    Runnable ar = new Runnable() {
+    	    public void run() {
+    	    		AdultItinerary(0);
+                }
+          };
+     for(int i=1; i<= children;i++){
+    	 KThread t = new KThread(cr);
+    	 t.setName("child"+i);
+    	 t.fork();
+     }
+     
+     for(int i=1; i<= adults;i++){
+    	 KThread t = new KThread(ar);
+    	 t.setName("adult"+i);
+    	 t.fork();
+     }
+     Runnable r = new Runnable() {
+ 	    public void run() {
+ 	    		while(OnMolokai != Total){
+ 	    			System.out.println("@.@ there are "+OnMolokai+" people on molokai already");
+ 	    		}
+ 	    		System.out.println("@.@ all people pass!");
+             }
+       };
+       KThread t = new KThread(r);
+  	 t.setName("main thread");
+  	 t.fork();
 	// Walkthrough linked from the projects page.
-
+	/*
 	Runnable r = new Runnable() {
 	    public void run() {
                 SampleItinerary();
@@ -38,10 +90,10 @@ public class Boat
         KThread t = new KThread(r);
         t.setName("Sample Boat Thread");
         t.fork();
-
+	*/
     }
 
-    static void AdultItinerary()
+    static void AdultItinerary(int loc)
     {
 	bg.initializeAdult(); //Required for autograder interface. Must be the first thing called.
 	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
@@ -52,12 +104,71 @@ public class Boat
 	       bg.AdultRowToMolokai();
 	   indicates that an adult has rowed the boat across to Molokai
 	*/
+	int Location = loc;
+	while(true){
+		BoatLock.acquire();
+		if(Location == 0){ //Oahu
+			while(BoatLocation != 0 || Weight == 100 ||ChOahu>1){
+				WaitOahu.sleep();
+			}
+			Weight += 100;
+			bg.AdultRowToMolokai();
+			BoatLocation = 1;
+			Location = 1;
+			OnMolokai += 1;
+			Weight -= 100;
+			WaitMolokai.wakeAll();
+			WaitMolokai.sleep();
+		}
+		else{//Molokai
+			WaitMolokai.sleep();
+		}
+		BoatLock.release();
+		}
     }
 
-    static void ChildItinerary()
+    static void ChildItinerary(int loc)
     {
 	bg.initializeChild(); //Required for autograder interface. Must be the first thing called.
 	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
+	
+	int Location = loc;
+	while(true){
+		BoatLock.acquire();
+		if(Location == 0){//Oahu
+			while(BoatLocation !=0 ||Weight==100||ChOahu==1){
+				WaitOahu.sleep();
+			}
+			WaitOahu.wakeAll();
+			if(Weight==0){// first
+				Weight += 50;
+				WaitFull.sleep();
+				bg.ChildRideToMolokai();
+				Location = 1;
+			}
+			else{
+				Weight += 50;
+				bg.ChildRowToMolokai();
+				WaitFull.wakeAll();// all or no all, same
+				ChOahu -= 2;
+				BoatLocation = 1;
+				Location = 1;
+				OnMolokai += 2;
+				Weight -= 100;
+				WaitMolokai.sleep();
+			}
+		}
+		else{//Molokai
+			Weight += 50;
+			OnMolokai -= 1;
+			BoatLocation = 0;
+			Location = 0;
+			Weight -= 50;
+			ChOahu += 1;
+			WaitOahu.wakeAll();
+		}
+		BoatLock.release();
+		}
     }
 
     static void SampleItinerary()
