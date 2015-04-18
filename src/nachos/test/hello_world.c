70411b3b7feeb274d7a563140ba834294b739647
FlameYan <yan-zp@foxmail.com>
2015-04-18 19:31:44 +0800

    tested MIPS cross-compiler

---
+++ src/nachos/test/hello_world.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv)
+{
+  int i;
+  FILE fout;
+  fout = creat("hello.txt");
+  fprintf(fout, "Hello world! %d args:\n", argc);
+  for(i=0;i<argc;i++) fprintf(fout, "%s\n", argv[i]);
+  close(fout);
+  return 0;
+}
