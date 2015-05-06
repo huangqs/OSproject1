6ac2d9f4df245cd7ad204a1123646d1ec16d62fb
songzy12 <644879054@qq.com>
2015-05-06 15:17:49 +0800

    create test

---
+++ src/nachos/test/create_test.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv)
+{
+  FILE fout;
+  int i;
+  for(i=0; i<13; i++){
+    printf("create returns %d\n", fout = creat("create_test.txt"));
+  }
+  printf("file descriptors should be used up now... (Press any key to continue)\n");
+  getch();
+  printf("create returns %d\n", fout = creat("create_test.txt"));
+  return 0;
+}
